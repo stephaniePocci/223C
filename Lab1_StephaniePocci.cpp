dedf863f700bf68f9c6b1ece5c43380d986545b9
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-03 09:48:37 -0800

    initial

---
+++ Lab1_StephaniePocci.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define LOWER 0 //define our lower limit
+#define UPPER 300 //define our upper limit
+#define STEPP 20 //define our step
+//program that converts from Fahrenheit to Celsius
+//0, 20, 40 ... 300
+int main()
+{
+  float fahr, celsius;
+  fahr = 0;
+  celsius = 0;
+  fahr = LOWER;
+
+  while(fahr <= UPPER)
+  {
+    //converting from fahreneit to celsius
+    celsius = 5.0 * (fahr-32.0) / 9.0;
+    fahr = celsius * (9.0/5.0) + 32;
+    printf("%3.0f %6.1f\n", fahr, celsius); //%d assumes base 10
+    //3.0f - print at least 3 characters wide with no decimal
+    //6.1f - print at least 6 characters wide with 1 digit after the decimal
+
+    //increase our tempature by 20
+    fahr = fahr + STEPP;
+  }
+
+
+  return 0;
+}
