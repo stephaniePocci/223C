aaf7c478323ed2a981ef68eac3c704a9143127a8
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-08 14:59:53 -0800

    added code

--- Lab2-Part1.c
+++ Lab2-Part1.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
 int main() {
-  int sales1 = 0;
-  int sales2 = 0;
-  int sales3 = 0;
+  float sales1 = 0;
+  float sales2 = 0;
+  float sales3 = 0;
   int i = 0;
   int count = 3;
 
-/*  while (i <= count) {
-    printf("Enter the sales for day ", %i);
-  }
-*/
-
   printf("Enter the sales for day 1: \n");
-  scanf("%d", &sales1);
+  scanf("%f", &sales1);
   printf("Enter the sales for day 2: \n");
-  scanf("%d", &sales2);
+  scanf("%f", &sales2);
   printf("Enter the sales for day 3: \n");
-  scanf("%d", &sales3);
+  scanf("%f", &sales3);
 
   printf("\n");
   printf("Sales Amounts\n");
   printf("--------------------------\n");
-  printf("Day 1:                   %d\n", &sales1);
+  printf("Day 1:%6.2f\n", sales1);
 
   //scanf("")
 return 0;
