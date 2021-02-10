2b2aa8599e4b6ae779c6597fba50f6d0c4dd9460
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-10 13:20:24 -0800

    Need to fix output

--- Lab2-Part2.c
+++ Lab2-Part2.c
@@ -4,13 +4,34 @@ int main() {
 
   char street[50], city[50], state[1];
   int place, zip;
+  char choice;
 
-  printf("Enter your street: ");
-  scanf("%49s[\n]", street);
-  while((getchar())!='\n');
+  while (choice != 'n') {
+    printf("Enter your street: ");
+    scanf("%49s[\n]", street);
+    while((getchar())!='\n');
 
-  printf("Enter the house or apartment number: ");
-  scanf("%d", &place);
-  while((getchar())!='\n');
+    printf("Enter the house or apartment number: ");
+    scanf("%d", &place);
+    while((getchar())!='\n');
+
+    printf("Enter the city: ");
+    scanf("%49s[\n]", city);
+    while((getchar())!='\n');
+
+    printf("Enter the zipcode: ");
+    scanf("%d[\n]", &zip);
+    while((getchar())!='\n');
+
+    printf("Enter the State: ");
+    scanf("%49s[\n]", state);
+    while((getchar())!='\n');
+
+    printf("%d %s, %s, %s %d\n", place, street, city, state, zip);
+
+    printf("Would you like to enter another address? (Enter y/n): \n");
+    scanf("%s", &choice);
+  }
+
   return 0;
 }
