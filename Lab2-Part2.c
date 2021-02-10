0890371c3df86bc28379631174733e97d556c1ec
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-10 12:49:51 -0800

    Added house/apt func

--- Lab2-Part2.c
+++ Lab2-Part2.c
@@ -2,9 +2,15 @@
 
 int main() {
 
-  char street[50], city[50],
+  char street[50], city[50], state[1];
+  int place, zip;
 
-  printf("Enter your street: ")
-  scanf
+  printf("Enter your street: ");
+  scanf("%49s[\n]", street);
+  while((getchar())!='\n');
+
+  printf("Enter the house or apartment number: ");
+  scanf("%d", &place);
+  while((getchar())!='\n');
   return 0;
 }
