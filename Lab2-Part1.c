776a79628bb52a54f793e3a96ac4903f273784cb
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-10 10:00:22 -0800

    Finished Part 1 of Lab 2

--- Lab2-Part1.c
+++ Lab2-Part1.c
@@ -4,6 +4,7 @@ int main() {
   float sales1 = 0;
   float sales2 = 0;
   float sales3 = 0;
+  float totalSales = 0;
   int i = 0;
   int count = 3;
 
@@ -14,10 +15,15 @@ int main() {
   printf("Enter the sales for day 3: \n");
   scanf("%f", &sales3);
 
+  totalSales = sales1 + sales2 + sales3;
+
   printf("\n");
   printf("Sales Amounts\n");
   printf("--------------------------\n");
-  printf("Day 1:%6.2f\n", sales1);
+  printf("Day 1:              %6.2f\n", sales1);
+  printf("Day 2:              %6.2f\n", sales2);
+  printf("Day 3:              %6.2f\n", sales3);
+  printf("Total:              %6.2f\n", totalSales);
 
   //scanf("")
 return 0;
