16a2f274113fc750e769f2a15dd0f1deaf3203f4
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-11 19:44:06 -0800

    Final Lab3

--- Lab3.c
+++ Lab3.c
@@ -7,30 +7,44 @@
 
 int main()
 {
-int nl, nw, nc, state, i;
-state = OUT;
+int nl, nw, nc, state;
+state = IN;
 nw = nc = 0;
 nl = 1;
 //char word[9];
 
-int c;
+int c, i, j;
+int arrayCharCount[10] = {0, 0, 0, 0, 0, 0, 0 ,0 ,0 ,0};
 
 printf("Enter a word or phrase (type q to quit): \n");
 
-while((c = getchar()) != 'q')
-{
-  ++nc; //counting the amount of characters
-  if(c=='\n')
-  ++nl; //counting the number of lines
-  if(c == ' ' || c == '\n' || c == '\t')
-  state = OUT;
-  else if(state==OUT)
+  while((c = getchar()) != 'q')
   {
-    state = IN;
-    ++nw;
+    if((c != ' ' && c != '\n' && c != '\t')) {
+      ++nc; //counting the amount of characters
   }
-}
+    if(c=='\n')
+    ++nl; //counting the number of lines
+    if(c == ' ' || c == '\n' || c == '\t'){
+      state = OUT;
+    } else if(state==OUT) {
+      state = IN;
+      nw++;
+      nc = 0;
+    }
+    if(nw > 0) {
+      arrayCharCount[nw] = nc + 1; //comp for getchar less than 1
+    } else { arrayCharCount[nw] = nc; }
+  }
 
+for(i = 0; i < 10; i++) {
+  printf("%d: ", (i+1));
+  for(j = 0; j < arrayCharCount[i]; j++) {
+    printf("=");
+  }
+  printf("\n");
+}
+/*
 for(i = 1; i <= nw; i++) {
   printf("%d: ", i);
   int newCount = 0;
@@ -40,6 +54,7 @@ for(i = 1; i <= nw; i++) {
   }
   printf("\n");
 }
+*/
 //printf("%d, %d, %d\n", nl, nw, nc);
 
 return 0;
