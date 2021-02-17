645199938c8071dd5ac05480400fb24d7dbe1972
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-17 13:08:03 -0800

    Need to correct Fibonacci Sequence to begin at 1

--- Lab4.c
+++ Lab4.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+  int i;
+  unsigned long int fibonacciNum, sum = 0;
+  unsigned long int num1, num2 = 1;
+  printf("Please enter the number of elements to be generated: \n");
+  scanf("%ld", &fibonacciNum);
+  printf("The first %ld elements of the Fibonacci sequence are: \n", fibonacciNum);
+
+  for(i = 0; i <= fibonacciNum; ++i) {
+    printf("%ld ", num1);
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return 0;
+}
