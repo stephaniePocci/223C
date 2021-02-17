6798bb50e8f7845ac7268f97d8521aa4f0cfa152
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-17 13:16:21 -0800

    Finished Lab4.c

--- Lab4.c
+++ Lab4.c
@@ -3,12 +3,13 @@
 int main() {
   int i;
   unsigned long int fibonacciNum, sum = 0;
-  unsigned long int num1, num2 = 1;
+  unsigned long int num2 = 1;
+  unsigned long int num1 = 1;
   printf("Please enter the number of elements to be generated: \n");
   scanf("%ld", &fibonacciNum);
   printf("The first %ld elements of the Fibonacci sequence are: \n", fibonacciNum);
 
-  for(i = 0; i <= fibonacciNum; ++i) {
+  for(i = 1; i <= fibonacciNum; ++i) {
     printf("%ld ", num1);
     sum = num1 + num2;
     num1 = num2;
