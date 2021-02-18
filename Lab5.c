4b23291e23ca2f4cf72ac31c8fb1ea73cbb2c040
stephaniePocci <spokey@csu.fullerton.edu>
2021-02-17 17:15:55 -0800

    Need to add main functions

--- Lab5.c
+++ Lab5.c
@@ -1,1 +1,11 @@
+#include <stidio.h>
+/*
+Directions:
+Part 1 - Make a function to copy one array into another
+Part 2 - Feel free to use getline function with copy function &
+Write a program that reads 3 lines of input, and prints the longest line
+*/
+int main() {
 
+  return 0;
+}
