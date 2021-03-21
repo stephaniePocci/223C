db9cdbc493c57e9b486cac6e6a0c804ee182ca74
stephaniePocci <spokey@csu.fullerton.edu>
2021-03-21 05:31:46 -0700

    Need to fix board display

--- Assignment1.c
+++ Assignment1.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+/*
+Instructions:
+Write a program that simulates Tic-Tac-Toe
+- Assume all normal vals
+- If all spaces are fileld and no player has a match the game results in "Cats Game"
+- Space cannot be selected if it is occupied
+
+Start by using a 2D array of chars
+Initialize array to char of "blank space"
+- will make assignment easier
+
+P1 & P2 should use same code
+*/
+char array[3][3] = {"O","O","O","O","O","O","O","O","O"}; //Declaring 2D array for new game
+
+void gameBoard() {
+  printf("%s | %s | %s", array[0][0], &array[0][1], &array[0][2]);
+}
+
+int main() {
+
+printf("Welcome to Tic-Tac-Toe\n");
+printf("======================\n");
+int playerTurn;
+
+  return 0;
+
+  gameBoard();
+}
